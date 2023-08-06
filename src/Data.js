5f5888569dd66734883e4e7d0fe49f0ae5c9c4b8
Zakhar Lobai <lobajzahar@gmail.com>
2023-08-06 23:42:13 +0200

    data changes

--- src/Data.js
+++ src/Data.js
@@ -23,8 +23,8 @@ export const services = [
 ];
 
 export const tours = [
-    {id: 1, image: tour1, date: 'august 26th, 2020', title: 'Tibet Adventure', info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.', location: 'china', duration: 6, cost: 2100,},
-    {id: 2, image: tour2, date: 'october 1th, 2020', title: 'best of java', info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.', location: 'indonesia', duration: 11, cost: 1400,},
-    {id: 3, image: tour3, date: 'september 15th, 2020', title: 'explore hong kong', info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.', location: 'hong kong', duration: 8, cost: 5000,},
-    {id: 4, image: tour4, date: 'december 5th, 2019', title: 'kenya highlights', info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.', location: 'kenya', duration: 20, cost: 3300,},
+    {id: 1, image: tour1, date: 'august 26th, 2024', title: 'Tibet Adventure', info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.', location: 'china', duration: 6, cost: 2100,},
+    {id: 2, image: tour2, date: 'october 1th, 2024', title: 'best of java', info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.', location: 'indonesia', duration: 11, cost: 1400,},
+    {id: 3, image: tour3, date: 'september 15th, 2023', title: 'explore hong kong', info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.', location: 'hong kong', duration: 8, cost: 5000,},
+    {id: 4, image: tour4, date: 'december 5th, 2025', title: 'kenya highlights', info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.', location: 'kenya', duration: 20, cost: 3300,},
 ];
